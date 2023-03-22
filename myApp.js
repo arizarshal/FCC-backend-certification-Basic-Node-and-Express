3b02cb3485fc1ca948a7df5987b500114199900f
Ariz Arshal <arizarshal@gmail.com>
2023-03-22 17:36:33 +0530

    middleware 2

--- myApp.js
+++ myApp.js
@@ -6,26 +6,26 @@ app.use("/public", express.static(__dirname + '/public'))
 
 absolutePath = __dirname + '/views/index.html'
 
-
-app.use(function(req, res, next) {
+const logger = function(req, res, next) {
     console.log(req.method, req.path,'-', req.ip)
     next()
-})
+}
+
+app.use(logger)
+
 
 app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 })
 
 
-app.get("/json", (req, res) => {
-    const mySecret = process.env["MESSAGE_STYLE"];
-    if (mySecret === "uppercase") {
-      response = "Hello json".toUpperCase();
-    } else {
-      response = "Hello json";
-    }
-    res.json({ message: response });
-  });
+app.get("/json", (req, res)=>{
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"})
+      } else {
+        res.json({"message": "Hello json"})
+      }
+})
 
 
  module.exports = app;
