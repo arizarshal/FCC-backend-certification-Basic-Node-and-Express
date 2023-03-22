aaea4392c88edb72ef2640d45bd95679335acdb2
Ariz Arshal <arizarshal@gmail.com>
2023-03-22 17:27:08 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -12,6 +12,10 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 })
 
+app.use(function(req, res, next) {
+    console.log(req.method, req.path,'-', req.ip)
+    next()
+})
 
 app.get("/json", (req, res) => {
     const mySecret = process.env["MESSAGE_STYLE"];
