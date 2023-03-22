9d578bbe4bc24094cc5a0d4585cd75d88541011a
Ariz Arshal <arizarshal@gmail.com>
2023-03-22 22:12:15 +0530

    reformatted code with proper comments

--- myApp.js
+++ myApp.js
@@ -3,18 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+// Exercise 11 - Use body-parser to Parse POST Requests
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Exercise 7:
+// Exercise 7 - Implement a Root-Level Request Logger Middleware
 app.use((request, response, next) => {
     console.log(request.method + " " + request.path + " - " + request.ip);
     next();
   });
 
-// Exercise 4
+// Exercise 4 - Serve Static Assets
 app.use("/public", express.static(__dirname + '/public'))
 
-// Exercise 3
+// Exercise 3 - Serve an HTML File
 absolutePath = __dirname + '/views/index.html'
 
 
@@ -23,9 +24,9 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 })
 
-// Exercise 5:
+// Exercise 5 - Serve JSON on a Specific Route
 app.get("/json", (req, res)=>{
-    // Exercise 6
+    // Exercise 6  - Use the .env File
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({"message": "HELLO JSON"})
       } else {
@@ -34,7 +35,7 @@ app.get("/json", (req, res)=>{
 })
 
 
-// Exercise 8
+// Exercise 8- Chain Middleware to Create a Time Server
 const time = (req, res, next)=>{
     req.time = new Date().toString()
     next()
@@ -47,10 +48,8 @@ app.get('/now', time, (req, res) => {
     }
 )
 
-// Exercise 9
+// Exercise 9 - Get Route Parameter Input from the Client
 app.get('/:word/echo', (req, res) => {
-    // const word = req.params.word
-    // console.log(word)
 
     res.send({
         echo: req.params.word
@@ -58,27 +57,23 @@ app.get('/:word/echo', (req, res) => {
 })
 
 
-// Exercise 10
+// Exercise 10 - Get Query Parameter Input from the Client
 app.route("/name").get(function(req, res) {
-    console.log(req.query)
-    const firstName = req.query.first;
-    const lastName = req.query.last;
+    const  { first: firstName, last: lastName } = req.query;
 
     res.json({
       name: `${firstName} ${lastName}`
     });
   })
+// Exercise 12 - Get Data from POST Requests
     .post((req, res) => {
-        const firstName = req.body.first;
-        const lastName = req.body.last;
+        const { first: firstName, last: lastName } = req.body
 
         res.json({
             name: `${firstName} ${lastName}`
         })
     })
-  ;
 
-// Exercise 11
 
 
 
