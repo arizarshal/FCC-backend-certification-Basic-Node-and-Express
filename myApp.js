457a280d9c7ac27eeb7f5242ae26f1d8d29db0a9
Ariz Arshal <arizarshal@gmail.com>
2023-03-22 17:28:30 +0530

    middleware 2

--- myApp.js
+++ myApp.js
@@ -7,15 +7,15 @@ app.use("/public", express.static(__dirname + '/public'))
 absolutePath = __dirname + '/views/index.html'
 
 
+app.use(function(req, res, next) {
+    console.log(req.method, req.path,'-', req.ip)
+    next()
+})
 
 app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 })
 
-app.use(function(req, res, next) {
-    console.log(req.method, req.path,'-', req.ip)
-    next()
-})
 
 app.get("/json", (req, res) => {
     const mySecret = process.env["MESSAGE_STYLE"];
