9248f40c4c679cf8df7a0c8364a48ec441886509
Ariz Arshal <arizarshal@gmail.com>
2023-03-22 21:04:53 +0530

    time middleware

--- myApp.js
+++ myApp.js
@@ -4,24 +4,27 @@ let app = express();
 
 
 
-// Exercise 4:
+// Exercise 7:
 app.use((request, response, next) => {
     console.log(request.method + " " + request.path + " - " + request.ip);
     next();
   });
 
+// Exercise 4
 app.use("/public", express.static(__dirname + '/public'))
 
+// Exercise 3
 absolutePath = __dirname + '/views/index.html'
 
 
-
+// Exercise 2, 3:
 app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 })
 
-
+// Exercise 5:
 app.get("/json", (req, res)=>{
+    // Exercise 6
     if (process.env.MESSAGE_STYLE === "uppercase") {
         res.json({"message": "HELLO JSON"})
       } else {
@@ -30,4 +33,16 @@ app.get("/json", (req, res)=>{
 })
 
 
+// Exercise 8
+const time = (req, res, next)=>{
+    req.time = new Date().toString()
+    next()
+}
+
+app.get('/now', time, (req, res) => {
+        res.send({
+            time: req.time
+        })
+    }
+)
  module.exports = app;
